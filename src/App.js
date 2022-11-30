550c224798441ed07492207e22e53742769322bc
James Murphy <jamesivdeveloper@gmail.com>
2022-11-30 10:30:38 -0600

    add authentication page component and sign in form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
-import SignIn from "./routes/sign-in/sign-in";
+import Authentication from "./routes/authentication/authentication";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
