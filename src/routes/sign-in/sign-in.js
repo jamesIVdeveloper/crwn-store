2e7a14e5cc104df070ab05915622c77ec4b97f3b
James Murphy <jamesivdeveloper@gmail.com>
2022-11-29 21:13:00 -0600

    add components sign-up, form-input, and button

--- src/routes/sign-in/sign-in.js
+++ src/routes/sign-in/sign-in.js
@@ -2,6 +2,7 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -12,7 +13,8 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with google popup</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
