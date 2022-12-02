9eb9feb5bb79b00ab703a39df2ea9ce331309ebf
James Murphy <jamesivdeveloper@gmail.com>
2022-12-02 11:46:41 -0500

    add addToCart faeture

--- src/routes/navigation/navigation.js
+++ src/routes/navigation/navigation.js
@@ -5,7 +5,7 @@ import CartIcon from "../../components/cart-icon/cart-icon";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown";
 
 import { UserContext } from "../../contexts/user.context";
-import { CartContext } from "../../contexts/card.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/imgs/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
